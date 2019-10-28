53c7a3258296f68a3642391846ff20ec6fbc9837
Javier Cervelló <javi.cervello@gmail.com>
2019-10-28 18:22:03 +0100

    Algunos arreglos

--- src/Ejercicio4/Main.kt
+++ src/Ejercicio4/Main.kt
@@ -2,10 +2,12 @@ package Ejercicio4
 
 fun main(){
    var lista :ArrayList<Empleado> = arrayListOf<Empleado>(Empleado("Javier", 2000F),Empleado("Jordi", 1999F),Empleado("Moises", 1999F))
-
+    var suma = 0F;
     for(i in lista){
+        suma+= i.sueldo;
         println(i.toString());
     }
+    println("Gasto total de sueldos: $suma");
     lista.add(Empleado("Juan",1200F))
 
     println("------------------------------------------");
@@ -13,8 +15,10 @@ fun main(){
     println("He añadido un nuevo empleado");
     println("------------------------------------------");
     println("------------------------------------------");
+    suma = 0F;
     for(i in lista){
+        suma += i.sueldo;
         println(i.toString());
     }
-
+    println("Gasto total de sueldos: $suma");
 }
